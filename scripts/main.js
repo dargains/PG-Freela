5f6ef0c3ec1e828ac38dbc876a301dd48bcf39b1
adargains <andre.dargains@gmail.com>
2016-06-12 11:13:27 +0100

    portfolio: lock/unlock screen

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,25 @@ $(document).ready(function () {
 
     $("article").click(function () {
         $(".popupPortfolio").show();
+        //trava o scroll
+        html.data('scroll-position', scrollPosition);
+        html.data('previous-overflow', html.css('overflow'));
+        html.css('overflow', 'hidden');
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+
     });
 
     $(".closePortfolio").click(function () {
         $(".popupPortfolio").hide();
+        //libera o scroll
+        var scrollPosition = html.data('scroll-position');
+        html.css('overflow', html.data('previous-overflow'));
+        window.scrollTo(scrollPosition[0], scrollPosition[1]);
     });
 });
+
+var scrollPosition = [
+  self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+  self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+];
+var html = $('html');
