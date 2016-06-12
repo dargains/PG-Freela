f5bfc75244f831155f0ec97d12a7660a1734254a
adargains <andre.dargains@gmail.com>
2016-06-12 20:08:58 +0100

    hero: hero-down funcionando

--- scripts/main.js
+++ scripts/main.js
@@ -18,10 +18,16 @@ $(document).ready(function () {
         html.css('overflow', html.data('previous-overflow'));
         window.scrollTo(scrollPosition[0], scrollPosition[1]);
     });
-});
 
-var scrollPosition = [
-  self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-  self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-];
-var html = $('html');
+    $(".hero-down").click(function(){
+        $('html,body').animate({
+        scrollTop: $('#bla').offset().top}, 'slow');
+    })
+
+    var scrollPosition = [
+      self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
+      self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
+    ];
+    var html = $('html');
+
+});
