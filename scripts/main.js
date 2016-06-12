7f838abb344facdbc11c0ec2e401962349ce0521
adargains <andre.dargains@gmail.com>
2016-06-12 22:24:16 +0100

    portfolio: scroll funcionando

--- scripts/main.js
+++ scripts/main.js
@@ -2,32 +2,59 @@ $(document).ready(function () {
     $(".popupPortfolio").hide();
 
     $("article").click(function () {
+        var html = $('html');
         $(".popupPortfolio").show();
         //trava o scroll
-        html.data('scroll-position', scrollPosition);
-        html.data('previous-overflow', html.css('overflow'));
-        html.css('overflow', 'hidden');
-        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+        disableScroll();
 
     });
 
     $(".closePortfolio").click(function () {
         $(".popupPortfolio").hide();
         //libera o scroll
-        var scrollPosition = html.data('scroll-position');
-        html.css('overflow', html.data('previous-overflow'));
-        window.scrollTo(scrollPosition[0], scrollPosition[1]);
+        enableScroll();
     });
 
     $(".hero-down").click(function(){
         $('html,body').animate({
         scrollTop: $('#hero-end').offset().top}, 'slow');
-    })
-
-    var scrollPosition = [
-      self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-      self.pageYOffset || document.documentElement.scrollTop  || document.body.scrollTop
-    ];
-    var html = $('html');
+    });
 
 });
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
