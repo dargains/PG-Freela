207a7489587b2705f418dd3379e231153ef90503
dpw1 <diego.boarutto.fortes@gmail.com>
2016-06-21 16:40:45 +0100

    internacionalizacao implementada

--- scripts/main.js
+++ scripts/main.js
@@ -78,6 +78,7 @@ $(document).ready(function () {
     var menuOpen = false;
 
     $('.menu_toggle').on('click', function () {
+
         $page.toggleClass('shazam');
 
         if ($($page).hasClass('shazam')) {
@@ -163,7 +164,10 @@ $(document).ready(function () {
         e.preventDefault();
         start = (start + add <= size_li) ? start + add : size_li;
         $('#expertise .row:lt(' + start + ')').slideDown(1500, function () {
-            $('#loadMore').text(phrase[click - 2]);
+            setTimeout(function () {
+                $('#loadMore').text(phrase[click - 2]);
+            }, 1500);
+            
         });
 
     });
@@ -183,19 +187,103 @@ $(document).ready(function () {
     aLangKeys['pt'] = new Array();
 
 
-    aLangKeys['en']['menu-about'] = 'Home';
-    aLangKeys['en']['menu-portfolio'] = 'Peoples >>';
-    aLangKeys['en']['menu-pros'] = 'All list';
-    aLangKeys['en']['menu-contact'] = 'Online';
-    aLangKeys['en']['hero-subtitle'] = 'english-test';
-    aLangKeys['en']['hero-btn'] = 'en :)';
 
-    aLangKeys['pt']['menu-about'] = 'Home';
+    aLangKeys['en']['menu-about'] = 'About us';
+    aLangKeys['en']['menu-portfolio'] = 'Portfolio';
+    aLangKeys['en']['menu-expertise'] = '';
+    aLangKeys['en']['menu-contact'] = '';
+    aLangKeys['en']['hero-h2'] = '';
+    aLangKeys['en']['about-h1'] = '';
+    aLangKeys['en']['about-h2'] = '';
+    aLangKeys['en']['pros-h1-1'] = '';
+    aLangKeys['en']['pros-h2-1'] = '';
+    aLangKeys['en']['pros-h1-2'] = '';
+    aLangKeys['en']['pros-h2-2'] = '';
+    aLangKeys['en']['pros-h1-3'] = '';
+    aLangKeys['en']['pros-h2-3'] = '';
+    aLangKeys['en']['pros-h1-4'] = '';
+    aLangKeys['en']['pros-h2-4'] = '';
+    aLangKeys['en']['quote-h1'] = '';
+    aLangKeys['en']['quote-action'] = '';
+    aLangKeys['en']['portfolio-h1'] = '';
+    aLangKeys['en']['portfolio-h2'] = '';
+    aLangKeys['en']['port-work-1-h1'] = '';
+    aLangKeys['en']['port-work-1-h2'] = '';
+    aLangKeys['en']['port-work-1-desc'] = '';
+    aLangKeys['en']['port-work-1-p'] = '';
+    aLangKeys['en']['expertise-h1'] = '';
+    aLangKeys['en']['expertise-h2'] = '';
+    aLangKeys['en']['expertise-reason-h1-1'] = '';
+    aLangKeys['en']['expertise-reason-h2-1'] = '';
+    aLangKeys['en']['expertise-reason-h1-2'] = '';
+    aLangKeys['en']['expertise-reason-h2-2'] = '';
+    aLangKeys['en']['expertise-reason-h1-3'] = '';
+    aLangKeys['en']['expertise-reason-h2-3'] = '';
+    aLangKeys['en']['expertise-reason-h1-4'] = '';
+    aLangKeys['en']['expertise-reason-h2-4'] = '';
+    aLangKeys['en']['expertise-reason-h1-5'] = '';
+    aLangKeys['en']['expertise-reason-h2-5'] = '';
+    aLangKeys['en']['expertise-reason-h1-6'] = '';
+    aLangKeys['en']['expertise-reason-h2-6'] = '';
+    aLangKeys['en']['quote-btn-more'] = '';
+    aLangKeys['en']['contact-h1'] = '';
+    aLangKeys['en']['contact-h2'] = '';
+    aLangKeys['en']['reach-us-h1-first'] = '';
+    aLangKeys['en']['reach-us-h2-first'] = '';
+    aLangKeys['en']['reach-us-h1-second'] = '';
+    aLangKeys['en']['reach-us-h2-second'] = '';
+    aLangKeys['en']['footer-p-1'] = '';
+    aLangKeys['en']['footer-p-2'] = '';
+
+
+
+    aLangKeys['pt']['menu-about'] = 'Sobre nós';
     aLangKeys['pt']['menu-portfolio'] = 'Portfólio';
-    aLangKeys['pt']['menu-pros'] = 'Todas listas';
-    aLangKeys['pt']['menu-contact'] = 'В сети';
-    aLangKeys['pt']['hero-subtitle'] = 'pt test';
-    aLangKeys['en']['hero-btn'] = 'ptptp';
+    aLangKeys['pt']['menu-expertise'] = 'Razões';
+    aLangKeys['pt']['menu-contact'] = ' Contato';
+    aLangKeys['pt']['hero-h2'] = 'Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo.';
+    aLangKeys['pt']['about-h1'] = 'Sobre nós';
+    aLangKeys['pt']['about-h2'] = 'Somos uma equipa de desevolvedores localizados em Lisboa Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, vero!';
+    aLangKeys['pt']['pros-h1-1'] = 'O que fazemos';
+    aLangKeys['pt']['pros-h2-1'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam illum, minima doloremque culpa magnam mollitia.';
+    aLangKeys['pt']['pros-h1-2'] = 'Nossos serviços';
+    aLangKeys['pt']['pros-h2-2'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam illum, minima doloremque culpa magnam mollitia.';
+    aLangKeys['pt']['pros-h1-3'] = 'Nossa paixão';
+    aLangKeys['pt']['pros-h2-3'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam illum, minima doloremque culpa magnam mollitia.';
+    aLangKeys['pt']['pros-h1-4'] = 'Profissionalismo';
+    aLangKeys['pt']['pros-h2-4'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam illum, minima doloremque culpa magnam mollitia.';
+    aLangKeys['pt']['quote-h1'] = 'We grow brands by making decisions that are rooted in business strategy.';
+    aLangKeys['pt']['quote-action'] = "Let's work together ";
+    aLangKeys['pt']['portfolio-h1'] = 'Portfólio';
+    aLangKeys['pt']['portfolio-h2'] = 'Nossos trabalhos mais recentes Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium neque porro tempore facere, culpa vel.';
+    aLangKeys['pt']['port-work-1-h1'] = 'Imagem 1';
+    aLangKeys['pt']['port-work-1-h2'] = 'Product industry - Lorem ipsum';
+    aLangKeys['pt']['port-work-1-desc'] = 'Web development + mobile website';
+    aLangKeys['pt']['port-work-1-p'] = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores earum quis, doloremque harum numquam hic illo nemo, rem dicta vel nisi ipsam adipisci provident dolor cupiditate incidunt cum minus! At.';
+    aLangKeys['pt']['expertise-h1'] = 'Por quê?';
+    aLangKeys['pt']['expertise-h2'] = 'Para que investir em marketing digital? Quais seriam os reais benefícios? Separamos abaixo algum dos principais motivos.';
+    aLangKeys['pt']['expertise-reason-h1-1'] = 'Reputação. Mostra para seus clientes a sua seriedade.';
+    aLangKeys['pt']['expertise-reason-h2-1'] = 'Um site visualmente coerente dá credibilidade ao seu trabalho e mostra que investe na sua própria empresa. Para isso, utilizamos conceitos avançados de usabilidade para gerar uma experiência agradável para os visitantes.';
+    aLangKeys['pt']['expertise-reason-h1-2'] = 'Visibilidade. Sua empresa sempre aberta, 24/7.';
+    aLangKeys['pt']['expertise-reason-h2-2'] = 'Potencialize novas oportunidades de negócios através de técnicas de SEO focadas na sua área de atuação, destacando-se em motores de busca como o Google, Bing e Yahoo. Sua empresa estará sempre acessível, mesmo em horários não comerciais.';
+    aLangKeys['pt']['expertise-reason-h1-3'] = 'Comunicação. Contato e feedback dinâmicos.';
+    aLangKeys['pt']['expertise-reason-h2-3'] = 'A maior parte das pessoas prefere comunicar-se virtualmente a ter que ligar ou visitar pessoalmente um sítio. Um endereço eletrônico é muito mais fácil de ser lembrado que um número telefônico. Manter um website para a sua empresa significa acesso à opinião dos seus clientes em tempo real, e caso perceba que algo não esteja a andar como deveria, poderá elaborar um plano de ação que melhor adeque-se à situação.';
+    aLangKeys['pt']['expertise-reason-h1-4'] = 'Concorrência. Sempre um passo à frente.';
+    aLangKeys['pt']['expertise-reason-h2-4'] = 'Ter uma presença virtual te deixará muito à frente daqueles que fazem o mesmo trabalho que você. Uma marca forte é capaz de inspirar a confiança necessária para que, na hora de investir seu dinheiro, o cliente escolha com assertividade a sua empresa.';
+    aLangKeys['pt']['expertise-reason-h1-5'] = 'Métricas. Meça a eficiência do seu website.';
+    aLangKeys['pt']['expertise-reason-h2-5'] = 'Prezamos por um website otimizado e oferecemos ferramentas que permitem-lhe aceder informações de tráfego em tempo real, ingressos por região geográfica, como o visitantes chegaram no seu website e quanto tempo eles permaneceram lá.';
+    aLangKeys['pt']['expertise-reason-h1-6'] = 'Expansão. Atinge novos horizontes.';
+    aLangKeys['pt']['expertise-reason-h2-6'] = 'Expande seu negócio independentemente da localização geográfica da sua empresa. Ter uma presença online significa que clientes podem aceder seu website de qualquer sítio do mundo. Porque permancer local se pode ser global?';
+    aLangKeys['pt']['quote-btn-more'] = 'Não estou convencido/a';
+    aLangKeys['pt']['contact-h1'] = 'Contato';
+    aLangKeys['pt']['contact-h2'] = 'Deixe-nos uma mensagem, nós somos amigáveis!';
+    aLangKeys['pt']['reach-us-h1-first'] = 'Fale conosco...';
+    aLangKeys['pt']['reach-us-h2-first'] = 'Sinta-se à vontade para nos enviar uma mensagem utilizando o formulário do site ou, se preferir, nos contactar via whatsapp, telemóvel, skype ou e-mail.';
+    aLangKeys['pt']['reach-us-h1-second'] = '...sem compromissos :)';
+    aLangKeys['pt']['reach-us-h2-second'] = 'Texto motivando cliente a contactar ';
+    aLangKeys['pt']['footer-p-1'] = 'Feito com';
+    aLangKeys['pt']['footer-p-2'] = 'por Studio Tagus :)';
+
 
     // onclick behavior
     $('.lang').click(function () {
@@ -204,6 +292,11 @@ $(document).ready(function () {
 
         // translate all elements that has a key
         $('*[key]').each(function (i) {
+            tagName = $(this).prop("tagName");
+            text = $(this).text();
+            //            console.log("<" + tagName + " key='" + $(this).attr('key') + "'>" + text + "</" + tagName + ">");
+            console.log(text);
+
             $(this).text(aLangKeys[lang][$(this).attr('key')]);
         });
 
