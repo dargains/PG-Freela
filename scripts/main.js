b531471c1adc04e92efb142e23863bb3e9ff1f0a
adargains <andre.dargains@gmail.com>
2016-06-26 22:43:53 +0100

    inicio portfolio slider

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
         window.location.href = 'project1.html';
     });
 
-
     // left: 37, up: 38, right: 39, down: 40,
     // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
     var keys = {
